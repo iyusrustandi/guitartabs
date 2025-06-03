41722291901ade8c9e87829be42a8bb46a02d3c4
iysrustandi <iyusrustandi2@gmail.com>
2025-06-03 10:26:13 +0700

    update tabs

--- scripts/lyrics-toggle.js
+++ scripts/lyrics-toggle.js
@@ -17,3 +17,33 @@ function setupToggleCheckbox(checkboxId, className) {
     toggleLyrics(className);
   });
 }
+
+//============== update script for hide class chords when class lyrics showed ===================
+
+// function toggleLyrics(id) {
+//   document.querySelectorAll(`.${id}`).forEach((element) => {
+//     element.classList.toggle('show');
+//   });
+// }
+
+// function setupToggleCheckbox(checkboxId, className) {
+//   const checkbox = document.getElementById(checkboxId);
+//   if (!checkbox) {
+//     console.error(`Checkbox with ID '${checkboxId}' not found.`);
+//     return;
+//   }
+
+//   checkbox.addEventListener('change', function () {
+//     const show = checkbox.checked;
+
+//     // Toggle class on lyrics / lyricsTabs
+//     document.querySelectorAll(`.${className}`).forEach((element) => {
+//       element.classList.toggle('show', show);
+//     });
+
+//     // Toggle visibility of .chords
+//     document.querySelectorAll('.chords').forEach((element) => {
+//       element.classList.toggle('hide', show);
+//     });
+//   });
+// }
